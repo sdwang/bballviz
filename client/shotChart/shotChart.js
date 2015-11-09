aec8f97b61e58ce89d86811d40d23743eba7548a
Si <siwang22@gmail.com>
2015-11-09 12:28:27 -0800

    (add) angular and angular-route to client

--- client/shotChart/shotChart.js
+++ client/shotChart/shotChart.js
@@ -2,4 +2,6 @@ angular.module('bballApp.shotChart', [])
 
 .controller('ShotChartController', function($scope) {
   $scope.test = 'this is a test message';
+
+  console.log('controller reached');
 })
