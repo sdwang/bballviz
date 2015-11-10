b1d086086d54a7391e29c72b5e23f88cc797c9b5
Si <siwang22@gmail.com>
2015-11-09 16:26:00 -0800

    (fix) button clicks go into correct state with $state.go

--- client/form/form.js
+++ client/form/form.js
@@ -1,12 +1,11 @@
 angular.module('bballApp.form', [])
 
-.controller('FormController', function($scope) {
+.controller('FormController', function($scope, $state) {
   
   $scope.statSelector = function() {
-    console.log(this.statType);
     var submitElement = angular.element( document.querySelector('#submit'));
-    console.log(submitElement);
     submitElement.attr('ui-sref', this.statType);
+    $state.go(this.statType);
   };
 
 });
