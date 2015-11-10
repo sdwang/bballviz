f957736a19ddd7376c74082a157128186823cd77
Si <siwang22@gmail.com>
2015-11-09 19:48:16 -0800

    (feature) Test GET request from client to server successful

--- server/server.js
+++ server/server.js
@@ -16,4 +16,25 @@ app.get('/', function(req, res) {
   res.render('index.html');
 });
 
+// app.get('http://stats.nba.com/stats/playerdashboardbyshootingsplits?*',
+//         function(req, res) {
+//           res.data = 'hello, request recieved by server';
+//           return res.data;
+//         })
+
+app.get('/test', function(req, res, next) {
+  console.log('getting to server');
+  res.status(200).send('hello');
+  next();
+});
+
+// app.use('/test', function(req, res, next) {
+//   // GET 'http://www.example.com/admin/new'
+//   console.log(req.originalUrl); // '/admin/new'
+//   console.log(req.baseUrl); // '/admin'
+//   console.log(req.path); // '/new'
+//   res.send(200, 'hello');
+//   next();
+// });
+
 module.exports = app;
