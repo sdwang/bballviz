90b0beab12625b8eb321c70c6ea9a4d578dff98b
Si <siwang22@gmail.com>
2015-11-10 12:38:02 -0800

    (debugging) highCharts unable to access data

--- client/shotChart/shotChart.js
+++ client/shotChart/shotChart.js
@@ -3,6 +3,9 @@ angular.module('bballApp.shotChart', [])
 .controller('ShotChartController', ['$scope', 'Utility', function($scope, Utility) {
   
   $scope.series = Utility.series;
+  $scope.missed = Utility.missed;
+  $scope.made = Utility.made;
+ 
 
   $(function () {
     $('#container').highcharts({
@@ -13,31 +16,35 @@ angular.module('bballApp.shotChart', [])
         },
 
         xAxis: {
-           lineWidth: 0,
-           minorGridLineWidth: 0,
-           lineColor: 'transparent',
-           labels: {
-               enabled: false
-           },
-           minorTickLength: 0,
-           tickLength: 0
+          min: -250,
+          max: 250,
+          lineWidth: 0,
+          minorGridLineWidth: 0,
+          lineColor: 'transparent',
+          labels: {
+            enabled: false
+          },
+          minorTickLength: 0,
+          tickLength: 0
         },
         yAxis: {
-           lineWidth: 0,
-           minorGridLineWidth: 0,
-           lineColor: 'transparent',
-           labels: {
-               enabled: false
-           },
-           minorTickLength: 0,
-           tickLength: 0,
-           gridLineColor: 'transparent',
-           title: {
-            text: '',
-            style: {
-              display: 'none'
-            }
+          min: -20,
+          max: 200,
+          lineWidth: 0,
+          minorGridLineWidth: 0,
+          lineColor: 'transparent',
+          labels: {
+              enabled: false
+          },
+          minorTickLength: 0,
+          tickLength: 0,
+          gridLineColor: 'transparent',
+          title: {
+           text: '',
+           style: {
+             display: 'none'
            }
+          }
         },
         title: {
           text: '',
@@ -75,11 +82,26 @@ angular.module('bballApp.shotChart', [])
                 }
             }
         },
-        series: $scope.series
-
+        //series: $scope.series
+          series: [{name: 'Shot\'s Missed',
+            color: 'rgba(223, 83, 83, .5)',
+            data: $scope.missed
+          }, {
+            name: 'Shot\'s Made',
+            color: 'rgba(119, 152, 191, .5)',
+            data: $scope.made
+          }]
       });
   });
 
+  //$('#container').highcharts().addSeries($scope.series);
+
   console.log($scope.series);
+
+//  console.log($scope.series[0]);
+
+  console.log('highcharts series', $('#container').highcharts().series);
+
+  
 
 }]);
