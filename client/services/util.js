dc06e7cd4be23e292c1dc6cf30c7253e1339fb68
Si <siwang22@gmail.com>
2015-11-09 19:58:48 -0800

    (feature) Client sends url with stat type, player ID, and year and receives response from server

--- client/services/util.js
+++ client/services/util.js
@@ -3,13 +3,11 @@ angular.module('bballApp.util', [])
 .factory('Utility', function($http) {
 
   var getShotChartData = function(playerID, year) {
-    console.log('getting to utility')
     return $http({
       method: 'GET',
-      // url: 'http://stats.nba.com/stats/playerdashboardbyshootingsplits?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlayerID='
-      //       + playerID + '&PlusMinus=N&Rank=N&Season='
-      //       + year + '&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&VsConference=&VsDivision='
-      url: '/test'
+      //url: 'http://stats.nba.com/stats/playerdashboardbyshootingsplits?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlayerID=' + playerID + '&PlusMinus=N&Rank=N&Season=' + year + '&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&VsConference=&VsDivision='
+      //url: '/test'
+      url: '/shotchartdata/' + playerID + '/' + year
     }).then(function(resp) {
       console.log(resp.data);
       return resp.data;
